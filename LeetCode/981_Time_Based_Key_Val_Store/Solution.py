4996b62d2fcab791bdc6aa165c4bf1087667f318
Alex <144410177+TheAlex7@users.noreply.github.com>
2025-05-02 00:06:56 -0700

    finished binary search problem

--- LeetCode/981_Time_Based_Key_Val_Store/Solution.py
+++ LeetCode/981_Time_Based_Key_Val_Store/Solution.py
@@ -1,10 +1,36 @@
+import bisect
 class Solution:
 
     def __init__(self):
-        pass
+        self.tm = dict()
+        self.prev_times_mp = dict() # finds existing keys
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        pass
+        if key in self.tm:
+            self.tm[key][timestamp] = value
+            bisect.insort(self.prev_times_mp[key], timestamp)
+        else:
+            self.tm[key] = {timestamp:value}
+            self.prev_times_mp[key] = [timestamp]
+        
 
     def get(self, key: str, timestamp: int) -> str:
-        pass
+        value = ""
+        if key not in self.tm:
+            return ""
+        for i in range(len(self.prev_times_mp[key])-1,-1,-1):
+            time = self.prev_times_mp[key][i]
+            if time <= timestamp:
+                value = self.tm[key][time]
+                break
+        return value
+    
+"""
+Approach: Brute Force
+
+Time:
+- set: O(n)
+- get: O(n)
+
+Space: O(m*n) where m is the number of keys and n is the number of unique timestamps
+"""
--- LeetCode/981_Time_Based_Key_Val_Store/unit_tests.py
+++ LeetCode/981_Time_Based_Key_Val_Store/unit_tests.py
@@ -7,12 +7,34 @@ if __name__ == "__main__":
 		# load inputs while omitting first line in test case file
 		inputs = [line.strip() for line in infile.readlines()][1:]
 
-	for i, (inputs,expected_output) in enumerate([tup.split("#") for tup in inputs]):
-		# process inputs further if necessary
+	for i, (calls,args,expected_output) in enumerate([tup.split("#") for tup in inputs]):
+		calls = [s.strip() for s in calls.replace("\"","").split(",")]
+		args = args.replace("\"","").split(".")
+		tups = []
+		for s in args:
+			s.replace("[", "")
+			s.replace("]", "")
+			temp_ls = []
+			for arg in s.split(","):
+				temp_ls.append(arg.strip())
+			tups.append(tuple(temp_ls))
+		expected_output = [out.strip() for out in expected_output.replace("\"","").split(",")]
 
-		# rename func() to appropriate function name, if needed
-		ans = Solution().func(inputs)
-		if str(ans) == expected_output:
+		test_obj = Solution()
+		ans = []
+		for i, call in enumerate(calls):
+			tup_args = tups[i]
+			out = "null"
+			if call == "TimeMap":
+				test_obj = Solution()
+			elif call == "set":
+				test_obj.set(tup_args[0],tup_args[1],tup_args[2])
+				out = "null"
+			elif call == "get":
+				out = test_obj.get(tup_args[0],tup_args[1])
+			ans.append(out)
+
+		if ans == expected_output:
 			passed = "PASSED"
 		else:
 			passed = "FAILED"
